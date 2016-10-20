8025ade47c47fa352ba76b43c86f150cef57c40f
Subash Thapa <subash@Subashs-MacBook-Pro.local>
2016-10-20 14:56:40 -0700

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Subash Thapa on 10/20/16.
+//  Copyright © 2016 Subash Thapa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
